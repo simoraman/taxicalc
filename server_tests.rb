f5291e3b8c4094f6f978a57decb296dd52a0c9bb
Simo Råman <simoraman@gmail.com>
2012-10-12 07:53:00 +0300

    added google maps.

--- server_tests.rb
+++ server_tests.rb
@@ -29,4 +29,9 @@ class AppTest < Test::Unit::TestCase
     result = JSON.parse(last_response.body)
     assert_equal(14.8, result['price'])
   end
+  def test_empty_params_should_not_fail
+    assert_nothing_raised do
+     post '/calculate', {:distance=>"", :passengers=>""}.to_json, "CONTENT_TYPE" => "application/json"  
+    end
+  end
 end
--- taxicalc_server.rb
+++ taxicalc_server.rb
@@ -2,18 +2,21 @@ require 'sinatra'
 require 'json'
 require './fare_calculator'
 require './price_class'
-#class Server < Sinatra::Base
-  set :public_folder, File.dirname(__FILE__) + '/front'
-	get '/' do
-	  send_file File.join(settings.public_folder, 'main.html')
-	end
-	
-	post '/calculate' do
-	  fare_calc=FareCalculator.new(PriceClass.new({1..2=>1.48, 3..4=>1.78, 5..6=>1.92, 7..10=>2.07}))
-	  data = JSON.parse request.body.read
-	  fare_calc.passengers = data['passengers'].to_f
-	  result = fare_calc.calculate(data['distance'].to_f)
-	  content_type :json
-  	{:price=> result}.to_json
+
+set :public_folder, File.dirname(__FILE__) + '/front'
+get '/' do
+  send_file File.join(settings.public_folder, 'main.html')
+end
+
+post '/calculate' do
+  fare_calc=FareCalculator.new(PriceClass.new({1..2=>1.48, 3..4=>1.78, 5..6=>1.92, 7..10=>2.07}))
+  begin
+    data = JSON.parse request.body.read
+  rescue
+    return {:error=>'malformed json'}.to_json
   end
-#end
+  fare_calc.passengers = data['passengers'].to_f
+  result = fare_calc.calculate(data['distance'].to_f)
+  content_type :json
+  {:price=> result}.to_json
+end
