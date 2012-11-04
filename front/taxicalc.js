c3c6344da7f629edbcc3f62d84277361ae658624
Simo Råman <simoraman@gmail.com>
2012-11-04 16:47:46 +0200

    Address autocompletion added. Route rendering added.

--- front/taxicalc.js
+++ front/taxicalc.js
@@ -1,7 +1,12 @@
+$(document).ready(function(){
+	initializeMap();
+});
+
 $('#distanceButton').click(function() { 
 	initializeDistanceMatrix($('#from').val(), $('#to').val());
 	return false;
 });
+
 $('#button').click(function() { 
 	var dataobject=JSON.stringify({ distance:$('#distance').text(), passengers:$('#passengers').val() });
 
@@ -20,15 +25,27 @@ $('#button').click(function() {
 	});
 	return false;
 });
+
+var markersArray = [];
+var map;
+var directionsDisplay;
+
 function initializeMap() {
         var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
+          center: new google.maps.LatLng(60.168058,24.94169),
           zoom: 8,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("map_canvas"),
+		var canvas=$('#map_canvas')[0];
+        map = new google.maps.Map(canvas,
             mapOptions);
+		directionsDisplay = new google.maps.DirectionsRenderer();
+		directionsDisplay.setMap(map);
+
+		new google.maps.places.Autocomplete($('#from')[0]);
+		new google.maps.places.Autocomplete($('#to')[0]);
 }
+
 function initializeDistanceMatrix(origin, destination){
 	var service = new google.maps.DistanceMatrixService();
 	service.getDistanceMatrix(
@@ -37,22 +54,35 @@ function initializeDistanceMatrix(origin, destination){
 	    destinations: [destination],
 	    travelMode: google.maps.TravelMode.DRIVING
 	  }, distanceCallback);
-
+	
+	var directionsService = new google.maps.DirectionsService();
+	var request = {
+		origin:origin,
+		destination:destination,
+		travelMode: google.maps.TravelMode.DRIVING
+	};
+	directionsService.route(request, function(result, status) {
+		if (status == google.maps.DirectionsStatus.OK) {
+			directionsDisplay.setDirections(result);
+		}
+	});
 	
 }
+
 function distanceCallback(response, status) {
 	if (status == google.maps.DistanceMatrixStatus.OK) {
 	    var origins = response.originAddresses;
 	    var destinations = response.destinationAddresses;
 
-	      var results = response.rows[0].elements;
-	      for (var j = 0; j < results.length; j++) {
-	        var element = results[j];
+	    var results = response.rows[0].elements;
+	    for (var j = 0; j < results.length; j++) {
+	    	var element = results[j];
 	        var distance = element.distance.text;
 	        var duration = element.duration.text;
 	        var from = origins[0];
 	        var to = destinations[0];
 	      	$('#distance').text(distance)
 	    }
+	
 	  }
 }
